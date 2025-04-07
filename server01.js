c43b8fb4841f714e71c2aaa3c645d44e8c47d20e
mrhid3 <gkornas@proton.me>
2025-04-07 09:01:44 +0200

    style, costam z contextem

--- server01.js
+++ server01.js
@@ -5,33 +5,35 @@ const hbs = require('express-handlebars');
 const formidable = require('formidable');
 const App = express()
 
-let current = "files"
-
 App.set('views', path.join(__dirname, 'views'));
 App.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));
 App.set('view engine', 'hbs');
 App.use(express.json());
 App.use(express.urlencoded({ extended: false }));
 
+baseDir = path.join(__dirname, "files")
+let context = [];
+
+
 function getDirectories(pathname){
     let folders = [];
-    fs.readdir(path.join(__dirname, pathname), (err, results)=> {
+    fs.readdir(path.join(baseDir), pathname), (err, results)=> {
         results.forEach(e => {
-            fs.lstat(path.join(__dirname, pathname, e), (err, stats)=>{
+            fs.lstat(path.join(baseDir, pathname, e), (err, stats)=>{
                 if (stats.isDirectory())
                     folders.push({"name": e})
                 }
             )
         })
-    })
+    }
     return folders;
 }
 
 function getFiles(pathname){
     let files = [];
-    fs.readdir(path.join(__dirname, pathname), (err, results)=> {
+    fs.readdir(path.join(baseDir, pathname), (err, results)=> {
         results.forEach(e => {
-            fs.lstat(path.join(__dirname, pathname, e), (err, stats)=>{
+            fs.lstat(path.join(baseDir, pathname, e), (err, stats)=>{
                 if (stats.isFile())
                     files.push({"name": e})
                 }
@@ -41,33 +43,53 @@ function getFiles(pathname){
     return files;
 }
 
+function getContext(){
+    let ret = ""
+    if (context.length > 0)
+        context.forEach((e) => {
+            ret = path.join(ret, e)
+        })
+    return ret;
+}
+
 App.get("/", (req, res) => {
     res.render("view.hbs", {
-        "folders": getDirectories(current),
-        "files": getFiles(current),
-        "deleteFile": "deleteFile()"
+        "folders": getDirectories(getContext()),
+        "files": getFiles(getContext()),
+        "deleteFile": "deleteFile()",
+        "context": [{folder: "skibidi"}]
     });
 });
 
 App.post("/createfolder", (req, res) => {
     let orig = req.body.call;
     let name = orig;
-    while(fs.existsSync(path.join(__dirname, current, name))){
+    while(fs.existsSync(path.join(__dirname, getContext(), name))){
         if(name.length > orig.length && name[name.length - 1] == ")"){
             name = orig + " (" + (Number(name[name.length - 2]) + 1) + ")";
         }else
             name = orig + " (1)"
     }
     console.log(name)
-    fs.mkdir(path.join(__dirname, current, name),
+    fs.mkdir(path.join(__dirname, getContext(), name),
         { recursive: false }, (err) => {});
     res.redirect("/");
 });
 
 App.post("/createfile", (req, res) => {
-    fs.appendFile(path.join(__dirname, current, req.body.call), "", (err) => {
-        console.log("skibidibi dab dab")
-    });
+    let orig = req.body.call;
+    let name = orig;
+    let split = name.length - 1;
+    for(; split--; split >= 0){
+        if(name[split] == ".")
+            break;
+    }
+    let i = 1;
+    while(fs.existsSync(path.join(__dirname, getContext(), name))){
+        name = orig.slice(0, split) + " (" + i + ")" + orig.slice(split)
+        i++;
+    }
+    fs.appendFile(path.join(__dirname, getContext(), name), "", (err) => {});
     res.redirect("/");
 });
 
@@ -76,19 +98,31 @@ App.post('/uploadfile', function (req, res) {
     form.keepExtensions = true;
     form.uploadDir = path.join(__dirname, current);
     form.parse(req, function (err, result, file) {
-        fs.rename(file.upload.path, path.join(__dirname, current,file.upload.name),
-            (err) => {});
+        let orig = file.upload.name;
+        let name = orig;
+        let split = name.length - 1;
+        for(; split--; split >= 0){
+            if(name[split] == ".")
+                break;
+        }
+        let i = 1;
+        while(fs.existsSync(path.join(__dirname, getContext(), name))){
+            name = orig.slice(0, split) + " (" + i + ")" + orig.slice(split)
+            i++;
+        }
+        fs.rename(file.upload.path, path.join(__dirname, getContext(), name), (err) => {});
         res.redirect("/");
     });
 });
 
 App.post('/deletefile', function (req, res) {
-    fs.unlink(path.join(__dirname, current, req.body.filename), (err)=>{});
+    console.log(path.join(baseDir, getContext(), req.body.filename))
+    fs.unlink(path.join(baseDir, getContext(), req.body.filename), (err)=>{});
     res.send("ok");
 })
 
 App.post('/deletefolder', function (req, res) {
-    fs.rmdir(path.join(__dirname, current, req.body.foldername), (err)=>{});
+    fs.rmdir(path.join(baseDir, getContext(), req.body.foldername), (err)=>{});
     res.send("ok");
 })
 
